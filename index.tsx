bc6b8f85509998d6cb9ba31cf7a6dfd92c3e2913
Bluscream <Bluscream@users.noreply.github.com>
2026-02-16 01:09:56 +0100

    Convention: fix description format (one sentence, no ending dot)

--- index.tsx
+++ index.tsx
@@ -18,7 +18,7 @@ import { isVoiceChannel } from "./utils/channels";
 export const pluginInfo = {
     id: "autoOpenVoiceTextChat",
     name: "Auto Open Voice Text Chat",
-    description: "Automatically opens the text chat of a voice channel when joining it.",
+    description: "Automatically opens the text chat of a voice channel when joining it",
     color: "#7289da",
     authors: [
         { name: "Bluscream", id: 1205616252488519723n },
